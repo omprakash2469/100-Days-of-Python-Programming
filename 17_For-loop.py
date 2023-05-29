51bbfdc231d3d0cc363659ae20f0b05b0e2036ed
om2469 <oprajapati249@gmail.com>
2023-05-29 22:38:58 +0530

    lession 17 for loops

---
+++ 17_For-loop.py
@@ -0,0 +1,19 @@
+# For Loop in python
+
+# For loop on string
+x = "Omprakash"
+# for i in x:
+#     print(i)
+
+# For loop on list
+names = ["Omprakash", "Paras", "Sahil"]
+# for name in names:
+#     print(name)
+
+# For loop with range
+# for i in range(1, 20):
+#     print(i)
+
+# For loop with step argument
+for i in range(1, 20, 3):
+    print(i)
