c7a2c13600452284f66a90ca10a9b2141570f0ef
om2469 <oprajapati249@gmail.com>
2023-05-30 23:21:14 +0530

    lession 18 while loop

---
+++ 18_While-loops.py
@@ -0,0 +1,13 @@
+# While Loop
+
+i = 0
+while(i<5):
+    print(i)
+    i+=1
+
+# Emulate while loop
+# while True:
+#     number = int(input("Enter a number: "))
+#     print(number)
+#     if number > 0:
+#         break
