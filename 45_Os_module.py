ab6c9f4505ff835a27755c401aefd1eab1b325a0
Omprakash Prajapati <oprajapati249@gmail.com>
2024-02-27 16:47:29 +0530

    os module

---
+++ 45_Os_module.py
@@ -0,0 +1,20 @@
+# OS Module
+# Os module is a built-in module in python  which provides a way of accessing system related information and  services. It allows you to interact with the operating
+
+import os
+
+# Creates a new directory - Throws error if the directory already exists
+# os.mkdir("data")
+
+# Create all directories in the path if not exists
+# os.makedirs('data/list')
+
+# List all the files in the directories
+# os.listdir("data")
+
+# Get current working directory
+# os.getcwd()
+
+# Change current working directory
+# os.chdir("data")
+# print(os.getcwd())
