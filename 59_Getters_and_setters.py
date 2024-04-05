d610d4998491d23c0304911e507089678ef37c07
Omprakash Prajapati <oprajapati249@gmail.com>
2024-04-05 16:59:25 +0530

    Getters and setters

---
+++ 59_Getters_and_setters.py
@@ -0,0 +1,25 @@
+"""
+Getters
+---
+Getters in Python are methods that are used to access the values of an object's properties. They are used to return the value of a specific property, and are typically defined using the @property decorator. Here is an example of a simple class with a getter method:
+
+Setters
+---
+It is important to note that the getters do not take any parameters and we cannot set the value through getter method.For that we need setter method which can be added by decorating method with @property_name.setter
+"""
+
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    @property # Getter
+    def get_name(self):
+        return self.name
+
+    @get_name.setter # Setter
+    def get_name(self, name):
+        self.name = name
+
+
+a = Person("Omprakash")
